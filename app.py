11e9ed0a7b202b4cf9d167e4936809f63e8bf37b
loomla <wittywys@gmail.com>
2013-10-27 18:51:22 +0800

    templates established

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/index/")
+def index():
+    return render_template("index.html",title="HOME")
+
+@app.route("/login", methods="GET")
+def login():
+    pass
+
+@app.route("/chat/")
+def chat():
+    return render_template("chat.html",title="chat room")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=80)
+    app.run(host="0.0.0.0",port=80, debug=True)
 
